23ff3cb687136ea2aae7687849e89de3ba5b99ec
Pawel <Pawel-9215@users.noreply.github.com>
2020-05-13 21:16:59 +0200

    Started main menu class
    I have no idea what I'm doing. At least testin "working copy" for ipad

--- main_menu.py
+++ main_menu.py
@@ -1,1 +1,22 @@
+#This module should contain classes to create somewhat of main_menu
+
 import curses
+
+class mainmenu():
+	menu_items = ["New Game", "Options", "Load Game", "Quit"]
+	win_y = 0
+	win_x = 0
+	win = None
+	focused_item = 0
+	
+	def __init__(self, window):
+		self.win = window
+		curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+		curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+		self.win.clear()
+		self.win.border()
+		
+		self.win_y, self.win_x = window.getmaxyx()
+		
+	def draw_item_list():
+		pass
