e41cf5f933f2d5da38c0b451f917ae25a9064b6c
Pawel-9215 <hordyniak@gmail.com>
2020-06-02 09:44:34 +0200

    Testing Py charm IDE

--- TerminalDungeon.py
+++ TerminalDungeon.py
@@ -1,4 +1,4 @@
-#This is main application body
+# This is main application body
 import curses
 import splash_screen
 import ui
@@ -11,16 +11,18 @@ from math import floor
 def main(wholescr):
     curses.curs_set(0)
     wholescr_y, wholescr_x = wholescr.getmaxyx()
-    wholescr.addstr(int(wholescr_y/2), int(wholescr_x/2), "".join([str(wholescr_y), " ", str(wholescr_x)]))
+    wholescr.addstr(int(wholescr_y / 2), int(wholescr_x / 2), "".join([str(wholescr_y), " ", str(wholescr_x)]))
     wholescr.refresh()
     curses.napms(300)
-    splash_screen.print_splash(wholescr_y, wholescr_x)  
+    splash_screen.print_splash(wholescr_y, wholescr_x)
     curses.napms(200)
 
-    ui_screen = curses.newwin(wholescr_y, floor(wholescr_x/3), 0, 0)
+    ui_screen = curses.newwin(wholescr_y, floor(wholescr_x / 3), 0, 0)
     ui_screen_maxy, ui_screen_maxx = ui_screen.getmaxyx()
     ui_screen.border()
     ui_screen.refresh()
+
+    # test git integration
 
     wholescr.clear()
     label1 = ui.Label(wholescr, "This is LABEL-1", 5, 5)
@@ -33,10 +35,7 @@ def main(wholescr):
     wholescr.refresh()
     curses.napms(1000)
 
-
-
-
-    game_map = curses.newwin(wholescr_y, wholescr_x-ui_screen_maxx, 0, ui_screen_maxx)
+    game_map = curses.newwin(wholescr_y, wholescr_x - ui_screen_maxx, 0, ui_screen_maxx)
     game_map_maxy, game_map_maxx = game_map.getmaxyx()
     game_map.border()
     game_map.refresh()
@@ -52,15 +51,16 @@ def main(wholescr):
     menu.draw_item_list()
     credits1 = main_menu.Credits([wholescr], wholescr, manager)
     credits1.print_content()
-    #Game conrol classes
+    # Game conrol classes
 
     colors = ui.ColorInit()
-    
-    render.scene=credits1
-    #update.updatepass("up")
+
+    render.scene = credits1
+    # update.updatepass("up")
     render.renderpass()
 
     curses.napms(2000)
+
 
 """
     wholescr.clear()
@@ -78,13 +78,5 @@ def main(wholescr):
         manager.updater.updatepass(input_control.last_pressed)
         manager.renderer.renderpass()
 """
-            
-            
-            
-    
-    
-    
 
 wrapper(main)
-
-
