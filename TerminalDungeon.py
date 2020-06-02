dce1b21284470ca58ca7d838eab9baa6711df168
Pawel-9215 <hordyniak@gmail.com>
2020-06-02 22:11:35 +0200

    Reorganizing project to be less spaghetti

--- TerminalDungeon.py
+++ TerminalDungeon.py
@@ -6,6 +6,7 @@ import game_control
 import main_menu
 from curses import wrapper
 from math import floor
+import engine
 
 
 def main(wholescr):
@@ -13,7 +14,7 @@ def main(wholescr):
     wholescr_y, wholescr_x = wholescr.getmaxyx()
     wholescr.addstr(int(wholescr_y / 2), int(wholescr_x / 2), "".join([str(wholescr_y), " ", str(wholescr_x)]))
     wholescr.refresh()
-    curses.napms(300)
+    curses.napms(600)
     splash_screen.print_splash(wholescr_y, wholescr_x)
     curses.napms(200)
 
@@ -22,61 +23,13 @@ def main(wholescr):
     ui_screen.border()
     ui_screen.refresh()
 
-    # test git integration
-
-    wholescr.clear()
-    label1 = ui.Label(wholescr, "This is LABEL-1", 5, 5)
-    label2 = ui.Label(wholescr, "This is LABEL-2", 6, 5)
-    label1.draw()
-    wholescr.refresh()
-    curses.napms(1000)
-    wholescr.clear()
-    label2.draw()
-    wholescr.refresh()
-    curses.napms(1000)
-
     game_map = curses.newwin(wholescr_y, wholescr_x - ui_screen_maxx, 0, ui_screen_maxx)
     game_map_maxy, game_map_maxx = game_map.getmaxyx()
     game_map.border()
     game_map.refresh()
-
-    render = game_control.Renderque()
-    update = game_control.Updateque()
-    input_control = game_control.Keyboard()
-
-    manager = game_control.Scene_Manager(render, update)
-    manager.input_controller = input_control
-
-    menu = main_menu.Mainmenu([wholescr], wholescr, manager)
-    menu.draw_item_list()
-    credits1 = main_menu.Credits([wholescr], wholescr, manager)
-    credits1.print_content()
-    # Game conrol classes
-
-    colors = ui.ColorInit()
-
-    render.scene = credits1
-    # update.updatepass("up")
-    render.renderpass()
-
     curses.napms(2000)
 
+    td_engine = engine.Engine(wholescr, ui_screen, game_map)
 
-"""
-    wholescr.clear()
-    #manager.change_scene(menu)
-    curses.napms(2000)
-    wholescr.clear()
-    manager.change_scene(credits1)
-    
-    curses.napms(2000)
-    wholescr.clear()
-    """
-"""
-    while True:
-        manager.input_controller.key_listen()
-        manager.updater.updatepass(input_control.last_pressed)
-        manager.renderer.renderpass()
-"""
 
 wrapper(main)
---
+++ engine.py
@@ -0,0 +1,9 @@
+# So this is supposed to be engine class.
+# We are going to store here a lot of shit :D
+
+class Engine():
+
+    def __init__(self, full_screen, left_bar, right_bar):
+        self.full_screen = full_screen
+        self.left_bar = left_bar
+        self.right_bar = right_bar
