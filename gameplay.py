1380fd49828dc3d4a99827cde253b6750ac90d76
Pawel-9215 <hordyniak@gmail.com>
2020-06-19 17:15:46 +0200

    Map population script WIP

--- gameplay.py
+++ gameplay.py
@@ -1,31 +1,14 @@
 import game_control
 import map_loader
 
-# Map classes
-
-class Cell():
-    def __init__(self, y, x, type, glyph, occupation="free"):
-        self.y = y
-        self.x = x
-        self.floor_type = "grass"
-        self.glyph = glyph
-        self.occupation = occupation
-
-class Body():
-    def __init__(self, y, x):
-        self.y = y
-        self.x = x
-        self.destructable = False
-        self.glyph = "@"
 
 
-class game_instance(game_control.Scene):
+
+class GameInstance(game_control.Scene):
     def __init__(self, windows, name: str, engine: object):
         super().__init__(windows, name, engine)
-        self.UI_window = self.windows[-1] #window for UI data
-        self.GP_window = self.windows[0] #Gameplay window - map and player
+        self.UI_window = self.windows[-1]  # window for UI data
+        self.GP_window = self.windows[0]  # Gameplay window - map and player
 
     def load_map(self):
         pass
-
-
--- map_loader.py
+++ map_loader.py
@@ -1,4 +1,6 @@
 import random
+import world_static
+
 
 def load_map(map_name):
     grid = []
@@ -8,11 +10,12 @@ def load_map(map_name):
     for line in map:
         grid.append(list(line.rstrip("\n")))
 
-    for y in range (1, len(grid)-1):
-        for x in range(1, len(grid[y])-1):
+    for y in range(1, len(grid) - 1):
+        for x in range(1, len(grid[y]) - 1):
             if grid[y][x] == "/":
-                grid[y][x]="█"
-            if grid[y][x]=="@" and (grid[y][x+1] == " " or grid[y][x-1]==" " or grid[y-1][x]==" " or grid[y+1][x]==" "):
+                grid[y][x] = "█"
+            if grid[y][x] == "@" and (
+                    grid[y][x + 1] == " " or grid[y][x - 1] == " " or grid[y - 1][x] == " " or grid[y + 1][x] == " "):
                 grid[y][x] = "█"
             if grid[y][x] == "P":
                 player_yx = [x, y]
@@ -20,14 +23,34 @@ def load_map(map_name):
     for y in range(1, len(grid) - 1):
         for x in range(1, len(grid[y]) - 1):
             if grid[y][x] == " ":
-                grid[y][x] = random.choice(floor)
+                grid[y][x] = " "
 
     return grid
+
 
 """
     for row in grid:
         print("".join(row))
 """
+
+
+def populate_map(map_name):
+    floor = [" ", ",", ".", " ", " ", ",", " ", " ", " "]
+    objects = {
+        "@":world_static.Cell(random.choice(floor), occupation=world_static.Rock),
+        "█":world_static.Cell(random.choice(floor), occupation=world_static.StoneWall),
+        " ":world_static.Cell(random.choice(floor), occupation="free")
+    }
+
+    base_grid = load_map(map_name)
+    populated_grid = []
+
+    for y in range(base_grid):
+        for x in range(base_grid[x]):
+            pass
+
+
+
 
 if __name__ == '__main__':
     map = load_map('Test_map_1')
