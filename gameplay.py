8be2757e1d9ee6e3b46e13a8b074c355a394c56b
Pawel-9215 <hordyniak@gmail.com>
2020-07-12 08:58:55 +0200

    Creating Character Sheet

--- gameplay.py
+++ gameplay.py
@@ -31,21 +31,22 @@ class GameInstance(game_control.Scene):
 
         grid_map = SituationMap(self.GP_window, self.grid, self)
         self.renderable_objects.append(grid_map)
-        character_info = Character_Sheet(self.UI_window, self)
+        character_info = CharacterSheet(self.UI_window, self)
         self.renderable_objects.append(character_info)
         self.updatable_objects.append(self.current_player)
 
-class Character_Sheet():
+
+class CharacterSheet():
     """
     UI info in the left window
     """
+
     def __init__(self, window, game_instance: GameInstance):
         self.window = window
         self.window_y, self.window_x = window.getmaxyx()
         self.game_instance = game_instance
 
     def draw(self):
-
         min_y = 1
         max_y = self.window_y - 1
         min_x = 1
@@ -53,8 +54,32 @@ class Character_Sheet():
         center_y = int(self.window_y / 2)
         center_x = int(self.window_x / 2)
 
-        self.window.addstr(1, center_x, "Hp:"+str(self.game_instance.current_player.health))
-        self.window.addstr(2, min_x, "Mel:"+str(self.game_instance.current_player.melee_skill))
+        self.window.addstr(1, center_x-3, "Hp:" + str(self.game_instance.current_player.health))
+        self.window.addstr(2, min_x, "Mel:" + str(self.game_instance.current_player.melee_skill))
+        self.window.addstr(2, max_x-len("Ran:" + str(self.game_instance.current_player.range_skill)),
+                           "Ran:" + str(self.game_instance.current_player.range_skill))
+        self.window.addstr(3, min_x, "Str:"+str(self.game_instance.current_player.strengh))
+        self.window.addstr(3, max_x-len("End:" + str(self.game_instance.current_player.endurance)),
+                           "End:" + str(self.game_instance.current_player.endurance))
+
+        self.window.addstr(4,
+                           int(center_x-len("Head:["+str(self.game_instance.current_player.arm_head)+"]")/2),
+                           "Head:["+str(self.game_instance.current_player.arm_head)+"]")
+        self.window.addstr(5,
+                           int(center_x - len("Torso:[" + str(self.game_instance.current_player.arm_torso) + "]") / 2),
+                           "Torso:[" + str(self.game_instance.current_player.arm_torso) + "]")
+        self.window.addstr(6,
+                           int(center_x - len("Hands:[" + str(self.game_instance.current_player.arm_hands) + "]") / 2),
+                           "Hands:[" + str(self.game_instance.current_player.arm_hands) + "]")
+        self.window.addstr(7,
+                           int(center_x - len("Legs:[" + str(self.game_instance.current_player.arm_legs) + "]") / 2),
+                           "Legs:[" + str(self.game_instance.current_player.arm_legs) + "]")
+
+        self.window.addstr(9,
+                           int(center_x - len("Weapon:[" + str(self.game_instance.current_player.weapon) + "]") / 2),
+                           "Weapon:[" + str(self.game_instance.current_player.weapon) + "]")
+        self.window.addstr(10, min_x, "Inventory:")
+        self.window.addstr(11, min_x, "1:["+str(self.game_instance.current_player.inv_1)+"]")
 
 class SituationMap:
     """
