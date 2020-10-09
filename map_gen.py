5aeb3dfb1a41b25c27f97398624bd2e3880cd1da
Pawel-9215 <hordyniak@gmail.com>
2020-10-09 15:40:03 +0200

    map generator WIP

--- map_gen.py
+++ map_gen.py
@@ -16,9 +16,9 @@ def generate_map(y_s, x_s):
 
     # vertical variant
 
-    x_margin = round(x_s/6)
+    x_margin = round(x_s/10)
     x_start = random.randint(0+x_margin, x_s - x_margin)
-    y_margin = round(y_s/8)
+    y_margin = round(y_s/10)
     y_start = y_s - y_margin
     iterations = round(x_s/10)
 
@@ -30,9 +30,12 @@ def generate_map(y_s, x_s):
         while current_cell[0] > y_margin:
 
             game_map[current_cell[0]][current_cell[1]] = " "
+            last_direction = "left"
 
-            if random.randint(1, 3) == 1:
+            if random.randint(1, 3) == 2:
                 go = "up"
+            elif random.randint(1, 4) == 3 and x_margin < current_cell[1] < x_s - x_margin:
+                go = last_direction
             else:
                 if random.randint(1, 99) <= ((current_cell[1]-x_margin)/(x_s-(2*x_margin)))*100:
                     go = "left"
@@ -41,10 +44,13 @@ def generate_map(y_s, x_s):
 
             if go == "up":
                 current_cell[0] -= 1
+                last_direction = "up"
             elif go == "left":
                 current_cell[1] -= 1
+                last_direction = "left"
             else:
                 current_cell[1] += 1
+                last_direction = "right"
 
     for row in game_map:
         print("".join(row))
